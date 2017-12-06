ea2e3a33cbdf83c19e7b767e1b9b2be97268dad6
LuggeKluring <bertilssonfoto@gmail.com>
2017-12-06 14:34:36 +0100

    Skicka ny kalender till databas
    + namn på användaren som skapat ny kalender i databasen

--- javaWindow/src/javaWindow/JavaWindow.java
+++ javaWindow/src/javaWindow/JavaWindow.java
@@ -4,13 +4,15 @@ import java.awt.Color;
 import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Insets;
+import java.awt.Font;
+//import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,12 +22,14 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.basic.BasicTabbedPaneUI;
 @SuppressWarnings("serial")
 
 
 public class JavaWindow extends JFrame
 {
+	public static String Username;
 	JLabel label;
 	JLabel pwdLabel;
 	JLabel welcometxt;
@@ -34,7 +38,7 @@ public class JavaWindow extends JFrame
 	JTextField txt = new JTextField();
 	JTextField calendarName = new JTextField();
 	JPasswordField pass = new JPasswordField();
-	JButton createCalendar = new JButton("Skapa ny kalender");
+	JButton createCalendar = new JButton("+ Skapa ny kalender");
 		//Ritar ut fönstret efter att lyckats logga in
 	public void drawMainWindow() 
 	{
@@ -44,9 +48,10 @@ public class JavaWindow extends JFrame
 		// ***
 		// *** Creates the JPanels and modifies them ***
 
-		welcometxt = new JLabel("Välkommen hit!");
+		welcometxt = new JLabel("Dina kalendrar");
 		rightTxt = new JLabel("Höger textelement");
-		JPanel leftSide = new JPanel(new BorderLayout());
+		JPanel leftSide = new JPanel();
+		
 		JTabbedPane rightSide = new JTabbedPane(JTabbedPane.TOP);
 		JPanel month = new JPanel();
 		JPanel week = new JPanel();
@@ -57,25 +62,44 @@ public class JavaWindow extends JFrame
 		topSide.setPreferredSize(new Dimension(1000, 100));
 		topSide.setBackground(darkGray);
 		topSide.setVisible(true);
-	
-		createCalendar.setPreferredSize(new Dimension(140,25));
+		topSide.setForeground(new Color(255,255,255));
+		
 
 		//rightSide
-		rightSide.setPreferredSize(new Dimension(750, 600));
+		rightSide.setPreferredSize(new Dimension(750, 500));
 		rightSide.setBackground(darkGray);
 		rightSide.setVisible(true);
+		rightSide.setForeground(new Color(255,255,255));
 		
 		//leftSide
+		leftSide.setLayout(new BoxLayout(leftSide, BoxLayout.PAGE_AXIS));
 		leftSide.add(welcometxt);
-		leftSide.setPreferredSize(new Dimension(250, 600));
+		welcometxt.setFont(new Font("Roboto", Font.BOLD, 30));
+		welcometxt.setBorder(new EmptyBorder(10,10,10,10));
+		leftSide.add(createCalendar);
+		leftSide.setPreferredSize(new Dimension(250, 500));
 		leftSide.setBackground(darkGray);
 		leftSide.setVisible(true);
+		createCalendar.setPreferredSize(new Dimension(140,25));
+		createCalendar.setBorderPainted(false);
+		createCalendar.setContentAreaFilled(false);
+		createCalendar.setForeground(new Color(255,255,255));
+		createCalendar.setFont(new Font("Roboto", Font.PLAIN, 13));
+		createCalendar.setBackground(null);
+		leftSide.setForeground(new Color(255,255,255));
+		
+		welcometxt.setForeground(new Color(255,255,255));
+		rightTxt.setForeground(new Color(255,255,255));
+		
+		
 		
 		//JTabbedPane
 		month.setBackground(gray);
 		week.setBackground(gray);
 		day.setBackground(gray);
 
+		
+		
 		month.add(rightTxt);
 		rightSide.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT); //sätter tabbarna till högra sidan
 		rightSide.setBorder(BorderFactory.createLineBorder(Color.darkGray, 0)); //Försöker sätta border color
@@ -95,11 +119,12 @@ public class JavaWindow extends JFrame
 			});
 		// ***
 		// *** Adds components to the Frame ***
-		topSide.add(createCalendar);
+		//leftSide.add(createCalendar);
 		rightSide.addTab("Dag", day);
 		rightSide.addTab("Vecka", week);
 		rightSide.addTab("Månad", month);
     
+		this.setForeground(new Color(255,255,255));
 		add(topSide, BorderLayout.NORTH);
 		add(leftSide, BorderLayout.WEST);
 		add(rightSide, BorderLayout.CENTER);
@@ -128,7 +153,7 @@ public class JavaWindow extends JFrame
 	
 	//skapar login-rutan
 	public void start() {
-		label = new JLabel("Användar-ID: ");
+		label = new JLabel("Användarnamn: ");
 		pwdLabel = new JLabel("Lösenord: ");
 		JTextField txt = new JTextField();
 		JPasswordField pass = new JPasswordField();
@@ -158,6 +183,7 @@ public class JavaWindow extends JFrame
 					String pswString = String.valueOf(pswChar);
 					//Kopplar till php filen
 					String str = "http://localhost/kalendersystem/kalendersystem.php?uNameSend="+txt.getText()+"&pswSend="+pswString;
+					str = str.replaceAll(" ", "%20");
 					String returnValue = db(str);
 					//kollar om fälterna är tomma eller ej
 				    if (!txt.getText().isEmpty() && !pswString.isEmpty()) 
@@ -166,7 +192,7 @@ public class JavaWindow extends JFrame
 						if(returnValue.equals("1")) 
 						{
 							//Skapar ny sträng som håller koll pä användarnamnet. 
-							String Username = txt.getText();		
+							Username = txt.getText();		
 							//Kallar på metoden som ritar ut kalender fönstret 
 							new JavaWindow().drawMainWindow();
 							System.out.println(Username);
@@ -196,7 +222,11 @@ public class JavaWindow extends JFrame
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
-
+	public void getCalendars() {
+		
+		
+		
+	}
 	//Detta är metoden får koppling till php filen, denna ser även till så att vi får returned data. 
 	public String db(String link) 
 	{
@@ -225,6 +255,7 @@ public class JavaWindow extends JFrame
 
 	public void createCalendar()
 	{
+		System.out.println(Username);
 		calendarNameLabel = new JLabel("Kalender namn: ");
 		JButton submitCalendarName = new JButton("Skapa kalender");
 		calendarName.setPreferredSize(new Dimension(200,30));
@@ -244,7 +275,7 @@ public class JavaWindow extends JFrame
 					if(!calendarName.getText().isEmpty())
 					{
 						System.out.println("Name: "+calendarName.getText());
-						String str = "http://localhost/kalendersystem/calendarCreate.php?calendarNameSend="+calendarName.getText();
+						String str = "http://localhost/kalendersystem/calendarCreate.php?uNameSend="+Username+"&calendarNameSend="+calendarName.getText();
 						str = str.replaceAll(" ", "%20");
 						System.out.println(str);
 						String returnValue = db(str);
@@ -269,6 +300,7 @@ public class JavaWindow extends JFrame
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			UIManager.getLookAndFeelDefaults().put("TabbedPane:TabbedPaneTab.contentMargins", new EmptyBorder(20, 20, 20, 20));
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (InstantiationException e) {
@@ -278,7 +310,7 @@ public class JavaWindow extends JFrame
 		} catch (UnsupportedLookAndFeelException e) {
 			e.printStackTrace();
 		}
-		UIManager.getLookAndFeelDefaults().put("TabbedPane:TabbedPaneTab.contentMargins", new Insets(30, 200, 0, 0));
+		
 		new JavaWindow().start();
 	}
 
